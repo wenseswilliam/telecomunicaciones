dc286b20135fdae9ae322ac428578b738b4b3b6d
Your Name <you@example.com>
2025-08-03 23:13:35 -0400

    creacion de modelos producto y detalle

---
+++ backend/app/Models/Detalle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Detalle extends Model
+{
+    //
+    Protected $table ='detalle';
+    protected $fillable=['nombre','precio','relacion con la marca'];
+}
