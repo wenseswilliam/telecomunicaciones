13ec460a5a79e3e4ee1b8a75be77f23992245813
Your Name <you@example.com>
2025-08-04 16:39:58 -0400

    login y registro de ususario

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\detalleController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
